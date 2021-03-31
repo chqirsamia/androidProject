ffcd41aacc9eea5013f1001c012effcff769d551
SAMIA CHQIR <samia_chqir@um5.ac.ma>
2021-03-31 17:34:43 +0100

    commit

--- app/src/main/java/com/example/mytest/RegisterActivity.java
+++ app/src/main/java/com/example/mytest/RegisterActivity.java
@@ -9,6 +9,12 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class RegisterActivity extends AppCompatActivity {
     EditText name,lastname,email,password,telephone,confirmpassword,datenaiss,cina,sex;
 
@@ -46,7 +52,7 @@ public class RegisterActivity extends AppCompatActivity {
                 String pass = password.getText().toString();
                 String repass = confirmpassword.getText().toString();
 
-                if(user.equals("")||pass.equals("")||repass.equals("") ||nom.equals("")||datenaissance.equals(""))
+                if(!validationdate(datenaissance)||!validationEmail(user,bdd)||pass.equals("")||repass.equals("") ||nom.equals("")||datenaissance.equals("") ||!validationMotDePasse(pass,repass))
                     Toast.makeText(RegisterActivity.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
                 else{
                     if(pass.equals(repass)){
@@ -79,7 +85,72 @@ public class RegisterActivity extends AppCompatActivity {
 
 
     }
-    public boolean isEmailValid(CharSequence email) {
-        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    private boolean validationdate(String date) {
+
+        boolean validation=true;
+        if ( date != null) {
+            if ( !date.matches( "[1]{1}[9]{1}[1-9]{2}[-][0]{1}[1-9]{1}[-][0-9]{1}[1-9]{1}" ) && !date.matches( "[1]{1}[9]{1}[1-9]{2}[-][1]{1}[0-2]{1}[-][0-9]{1}[1-9]{1}" )) {
+                Toast.makeText(RegisterActivity.this, "Merci de saisir une date valide.\"", Toast.LENGTH_SHORT).show();;
+                validation=false;
+            }/*else {
+                Date dateact = new Date();
+                Date datenais = new Date();
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                String dateactu = sdf.format(dateact);
+
+                try{
+                    //Définir la date
+                    datenais=sdf.parse(date);
+                    if (date.compareTo(dateactu)>0)
+                        Toast.makeText(RegisterActivity.this, "Cette date n'existe pas.\"", Toast.LENGTH_SHORT).show();;
+
+                    validation=false;
+
+                }catch(ParseException e){
+                    e.printStackTrace();
+                }
+            }*/
+        }
+            else  {
+            Toast.makeText(RegisterActivity.this, "Merci de saisir une date.", Toast.LENGTH_SHORT).show();;
+            validation=false;
+        }
+        return validation;
     }
+    private boolean validationEmail(String email,database bdd) {
+        Boolean checkuser = bdd.checkemail(email);
+        boolean validation=true;
+        if ( email != null) {
+            if ( !email.matches( "([^.@]+)(\\.[^.@]+)*@([^.@]+\\.)+([^.@]+)" ) )  {
+                Toast.makeText(RegisterActivity.this, "Merci de saisir une adresse mail valide.", Toast.LENGTH_SHORT).show();;
+                validation=false;
+            }else if ( checkuser ) {
+                Toast.makeText(RegisterActivity.this, "Cette adresse email est déjà utilisée, merci d'en choisir une autre.", Toast.LENGTH_SHORT).show();;
+
+                validation=false;
+            }
+        } else {
+            Toast.makeText(RegisterActivity.this, "Merci de saisir une adresse mail.", Toast.LENGTH_SHORT).show();
+validation=false;
+        }
+        return validation;
+    }
+    private boolean validationMotDePasse(String motDePasse,String confirmation){
+        boolean validation=true;
+        if (motDePasse != null && confirmation != null) {
+            if (!motDePasse.equals(confirmation)) {
+                Toast.makeText(RegisterActivity.this, "Les mots de passe entrés sont différents, merci de les saisir à nouveau.", Toast.LENGTH_SHORT).show();
+                validation=false;
+            } else if (motDePasse.trim().length() < 3) {
+                Toast.makeText(RegisterActivity.this, "Les mots de passe doivent contenir au moins 3 caractères.", Toast.LENGTH_SHORT).show();;
+                validation=false;
+            }
+        } else {
+            Toast.makeText(RegisterActivity.this, "Merci de saisir et confirmer votre mot de passe.", Toast.LENGTH_SHORT).show();;
+            validation=false;
+
+        }
+        return validation;
+    }
+
 }

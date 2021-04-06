35412fe066461afcd338733ec4c95794f0b8fc8e
SAMIA CHQIR <samia_chqir@um5.ac.ma>
2021-04-06 09:53:58 +0100

    commit

--- app/src/main/java/com/example/mytest/MainActivity.java
+++ app/src/main/java/com/example/mytest/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.mytest;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,14 +19,19 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 import bean.User;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private CircleImageView cinpicrecto,cinpicverso;
+    private static final int pickpicrecto=1,pickpicverso=2;
+    Uri imageUrirecto,imageUriverso;
     EditText telephone, cina, sex;
     Button signup;
     RadioButton homme, femme;
@@ -36,7 +50,8 @@ public class MainActivity extends AppCompatActivity {
         femme = (RadioButton) findViewById(R.id.femme);
         group = (RadioGroup) findViewById(R.id.gender);
         bdd = new database(this);
-
+        cinpicrecto = (CircleImageView) findViewById(R.id.cinpicrecto);
+        cinpicverso = (CircleImageView) findViewById(R.id.cinpicverso);
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -103,7 +118,27 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+        cinpicrecto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*Intent gallerie=new Intent();
+                gallerie.setType("image/*");
+                gallerie.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(gallerie,"choisir une photo"),pickpicrecto);*/
+                selectImage(MainActivity.this,"recto");
+            }
+        });
+        cinpicverso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               /* Intent gallerie=new Intent();
+                gallerie.setType("image/*");
+                gallerie.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(gallerie,"choisir une photo"),pickpicverso);*/
+                selectImage(MainActivity.this,"verso");
 
+            }
+        });
     }
 
     public User inscrireUser(String email, String password, String repass, String name, String lastname
@@ -189,5 +224,141 @@ public class MainActivity extends AppCompatActivity {
         }
         return validation;
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, requestCode, data);
+        /*if (requestcode == pickpicrecto && resultcode == RESULT_OK) ;
+        imageUrirecto = data.getData();
+        try {
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUrirecto);
+            cinpicrecto.setImageBitmap(bitmap);
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if(requestcode==pickpicverso && resultcode== RESULT_OK);
+        imageUriverso=data.getData();
+        try {
+            Bitmap bitmap= MediaStore.Images.Media.getBitmap(getContentResolver(),imageUriverso);
+            cinpicverso.setImageBitmap(bitmap);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+        if(resultCode != RESULT_CANCELED) {
+            switch (requestCode) {
+                case 0:
+                    if (resultCode == RESULT_OK && data != null) {
+                        Bitmap selectedImage = (Bitmap) data.getExtras().get("data");
+                        cinpicverso.setImageBitmap(selectedImage);
+                    }
+
+                    break;
+                case 1:
+                    if (resultCode == RESULT_OK && data != null) {
+                      /*  Uri selectedImage =  data.getData();
+                        String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                        if (selectedImage != null) {
+                            Cursor cursor = getContentResolver().query(selectedImage,
+                                    filePathColumn, null, null, null);
+                            if (cursor != null) {
+                                cursor.moveToFirst();
+
+                                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                                String picturePath = cursor.getString(columnIndex);
+                                cinpicverso.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+                                cursor.close();
+                            }
+                        }*/
+                        Intent gallerie=new Intent();
+                        gallerie.setType("image/*");
+                        gallerie.setAction(Intent.ACTION_GET_CONTENT);
+                        imageUrirecto=data.getData();
+                        try {
+                            Bitmap bitmap= MediaStore.Images.Media.getBitmap(getContentResolver(),imageUrirecto);
+                            cinpicrecto.setImageBitmap(bitmap);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                    break;
+                case 3:
+                    if (resultCode == RESULT_OK && data != null) {
+                        Bitmap selectedImage = (Bitmap) data.getExtras().get("data");
+                        cinpicrecto.setImageBitmap(selectedImage);
+                    }
+
+                    break;
+                case 4:
+                    if (resultCode == RESULT_OK && data != null) {
+                      /*  Uri selectedImage =  data.getData();
+                        String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                        if (selectedImage != null) {
+                            Cursor cursor = getContentResolver().query(selectedImage,
+                                    filePathColumn, null, null, null);
+                            if (cursor != null) {
+                                cursor.moveToFirst();
+
+                                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                                String picturePath = cursor.getString(columnIndex);
+                                cinpicrecto.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+                                cursor.close();
+                            }
+                        }*/ Intent gallerie=new Intent();
+                        gallerie.setType("image/*");
+                        gallerie.setAction(Intent.ACTION_GET_CONTENT);
+                        imageUriverso=data.getData();
+                        try {
+                            Bitmap bitmap= MediaStore.Images.Media.getBitmap(getContentResolver(),imageUriverso);
+                            cinpicverso.setImageBitmap(bitmap);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                    break;
+            }
+        }
+    }
+
+    private void selectImage(Context context,String cin) {
+        final CharSequence[] options = { "Take Photo", "Choose from Gallery","Cancel" };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Choose  picture");
+
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+
+                if (options[item].equals("Take Photo")) {
+                    Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    if(cin.equals("recto"))
+                    {
+                        startActivityForResult(takePicture, 0);}
+                    else
+                    {
+                        startActivityForResult(takePicture, 3);
+                    }
+
+                } else if (options[item].equals("Choose from Gallery")) {
+                    Intent pickPhoto = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    if(cin.equals("recto"))
+                    {
+                        startActivityForResult(pickPhoto , 1);}
+                    else{
+                        startActivityForResult(pickPhoto , 4);
+                    }
+
+                } else if (options[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
 }
--- app/src/main/java/com/example/mytest/RegisterActivity.java
+++ app/src/main/java/com/example/mytest/RegisterActivity.java
@@ -1,7 +1,16 @@
 package com.example.mytest;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,12 +19,18 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class RegisterActivity extends AppCompatActivity {
+    private CircleImageView profilepic;
+    private static final int pickpic=1;
+    Uri imageUri;
     EditText name,lastname,email,password,telephone,confirmpassword,datenaiss,cina,sex;
 
     Button signup;
@@ -23,22 +38,24 @@ public class RegisterActivity extends AppCompatActivity {
     RadioGroup group;
     database bdd;
     Button next;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         getSupportActionBar().setTitle("s'identifier");
-        name=(EditText) findViewById(R.id.name);
-        lastname=(EditText) findViewById(R.id.lastname);
-        email=(EditText) findViewById(R.id.email);
+        name = (EditText) findViewById(R.id.name);
+        lastname = (EditText) findViewById(R.id.lastname);
+        email = (EditText) findViewById(R.id.email);
 
-        datenaiss=(EditText) findViewById(R.id.datenaiss);
+        datenaiss = (EditText) findViewById(R.id.datenaiss);
 
-        password=(EditText) findViewById(R.id.password);
-        confirmpassword=(EditText) findViewById(R.id.confirmpassword);
-        next=(Button) findViewById(R.id.suivantbutton);
+        password = (EditText) findViewById(R.id.password);
+        confirmpassword = (EditText) findViewById(R.id.confirmpassword);
+        next = (Button) findViewById(R.id.suivantbutton);
 
         bdd = new database(this);
+        profilepic = (CircleImageView) findViewById(R.id.profile);
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,50 +63,58 @@ public class RegisterActivity extends AppCompatActivity {
                 String nom = lastname.getText().toString();
                 String user = email.getText().toString();
                 //String phone = telephone.getText().toString();
-                String datenaissance= datenaiss.getText().toString();
-               // String cin = cina.getText().toString();
+                String datenaissance = datenaiss.getText().toString();
+                // String cin = cina.getText().toString();
                 //String sexe ;
                 String pass = password.getText().toString();
                 String repass = confirmpassword.getText().toString();
 
-                if(!validationdate(datenaissance)||!validationEmail(user,bdd)||pass.equals("")||repass.equals("") ||nom.equals("")||datenaissance.equals("") ||!validationMotDePasse(pass,repass))
+                if (!validationdate(datenaissance) || !validationEmail(user, bdd) || pass.equals("") || repass.equals("") || nom.equals("") || datenaissance.equals("") || !validationMotDePasse(pass, repass))
                     Toast.makeText(RegisterActivity.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
-                else{
-                    if(pass.equals(repass)){
+                else {
+                    if (pass.equals(repass)) {
                         Boolean checkuser = bdd.checkemail(user);
-                        if(checkuser==false){
+                        if (checkuser == false) {
                             //Boolean insert = bdd.insertData1(user,pass,prenom,nom ,"client",datenaissance);
                             //if(insert==true){
-                                Toast.makeText(RegisterActivity.this, "veuillez terminer le formulaire", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                                intent.putExtra("prenom",prenom);
-                                intent.putExtra("nom", nom);
-                                intent.putExtra("user", user);
-                                intent.putExtra("datenaissance", datenaissance);
-                                intent.putExtra("pass", pass);
-                                intent.putExtra("repass", repass);
-                                startActivity(intent);
+                            Toast.makeText(RegisterActivity.this, "veuillez terminer le formulaire", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            intent.putExtra("prenom", prenom);
+                            intent.putExtra("nom", nom);
+                            intent.putExtra("user", user);
+                            intent.putExtra("datenaissance", datenaissance);
+                            intent.putExtra("pass", pass);
+                            intent.putExtra("repass", repass);
+                            startActivity(intent);
                            /* }else{
                                 Toast.makeText(RegisterActivity.this, "Registration failed", Toast.LENGTH_SHORT).show();
                             }*/
-                        }
-                        else{
+                        } else {
                             Toast.makeText(RegisterActivity.this, "User already exists! please sign in", Toast.LENGTH_SHORT).show();
                         }
-                    }else{
+                    } else {
                         Toast.makeText(RegisterActivity.this, "Passwords not matching", Toast.LENGTH_SHORT).show();
                     }
                 }
             }
         });
+        profilepic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            /*Intent gallerie=new Intent();
+            gallerie.setType("image/*");
+            gallerie.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(gallerie,"choisir une photo"),pickpic);*/
+                selectImage(RegisterActivity.this);
 
-
+            }
+        });
     }
     private boolean validationdate(String date) {
 
         boolean validation=true;
         if ( date != null) {
-            if ( !date.matches( "[1]{1}[9]{1}[1-9]{2}[-][0]{1}[1-9]{1}[-][0-9]{1}[1-9]{1}[ ]* " ) && !date.matches( "[1]{1}[9]{1}[1-9]{2}[-][1]{1}[0-2]{1}[-][0-9]{1}[1-9]{1}" )) {
+            if ( !date.matches( "[2]{1}[0]{1}[1-9]{2}[-][0]{1}[1-9]{1}[-][0-9]{1}[1-9]{1}[ ]* " ) &&!date.matches("[1]{1}[9]{1}[1-9]{2}[-][0]{1}[1-9]{1}[-][0-9]{1}[1-9]{1}[ ]* " ) && !date.matches( "[1]{1}[9]{1}[1-9]{2}[-][1]{1}[0-2]{1}[-][0-9]{1}[1-9]{1}" ) && !date.matches( "[2]{1}[0]{1}[1-9]{2}[-][1]{1}[0-2]{1}[-][0-9]{1}[1-9]{1}" )) {
                 Toast.makeText(RegisterActivity.this, "Merci de saisir une date valide.\"", Toast.LENGTH_SHORT).show();;
                 validation=false;
             }/*else {
@@ -153,4 +178,86 @@ validation=false;
         return validation;
     }
 
+    @Override
+public void onActivityResult(int requestCode, int resultCode, Intent data)
+{
+    super.onActivityResult(requestCode,requestCode,data);
+   /* if(requestcode==pickpic && resultcode== RESULT_OK);
+    imageUri=data.getData();
+    try {
+        Bitmap bitmap= MediaStore.Images.Media.getBitmap(getContentResolver(),imageUri);
+        profilepic.setImageBitmap(bitmap);
+    } catch (IOException e) {
+        e.printStackTrace();
+    }*/
+    if(resultCode != RESULT_CANCELED) {
+        switch (requestCode) {
+            case 0:
+                if (resultCode == RESULT_OK && data != null) {
+                    Bitmap selectedImage = (Bitmap) data.getExtras().get("data");
+                    profilepic.setImageBitmap(selectedImage);
+                }
+
+                break;
+            case 1:
+                Intent gallerie=new Intent();
+                gallerie.setType("image/*");
+                gallerie.setAction(Intent.ACTION_GET_CONTENT);
+                imageUri=data.getData();
+                try {
+                    Bitmap bitmap= MediaStore.Images.Media.getBitmap(getContentResolver(),imageUri);
+                    profilepic.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+              /*  if (resultCode == RESULT_OK && data != null) {
+                    Uri selectedImage =  data.getData();
+                    String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                    if (selectedImage != null) {
+                        Cursor cursor = getContentResolver().query(selectedImage,
+                                filePathColumn, null, null, null);
+                        if (cursor != null) {
+                            cursor.moveToFirst();
+
+                            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                            String picturePath = cursor.getString(columnIndex);
+                            profilepic.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+                            cursor.close();
+                        }
+                    }
+
+                }*/
+                break;
+}}}
+    private void selectImage(Context context) {
+        final CharSequence[] options = { "Take Photo", "Choose from Gallery","Cancel" };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Choose  picture");
+
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+
+                if (options[item].equals("Take Photo")) {
+                    Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+
+                        startActivityForResult(takePicture, 0);
+
+                } else if (options[item].equals("Choose from Gallery")) {
+                    Intent pickPhoto = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                        startActivityForResult(pickPhoto , 1);
+
+                    }
+
+                 else if (options[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
 }

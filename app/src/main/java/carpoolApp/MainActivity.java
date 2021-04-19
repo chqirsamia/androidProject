79f4a5460ab2057ce0184fe68af1cad1c1c3196c
Rihab-Btr <Rihab.btr@gmail.com>
2021-04-19 15:00:23 +0000

    avc

---
+++ app/src/main/java/carpoolApp/MainActivity.java
@@ -0,0 +1,31 @@
+package carpoolApp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button search_rides;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        search_rides = findViewById(R.id.search_carpool);
+        search_rides.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
